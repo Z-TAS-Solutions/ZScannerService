f55cb520c60e865b660e5407ae96b8d2de82afca
zischl <supermechs456@gmail.com>
2026-03-25 15:24:41 +0530

    - Added new zpi client node mode

---
+++ api/grpc/zpi_client.go
@@ -0,0 +1,20 @@
+
+import (
+	"log"
+
+	"github.com/Z-TAS-Solutions/ZScannerService/internal/pkg/zscanproto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func RunZPiClient(ip string) zscanproto.ZPiControllerClient {
+	conn, err := grpc.Dial(ip, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+
+	client := zscanproto.NewZPiControllerClient(conn)
+	log.Print("Connected to ZPi GRPC Host!")
+
+	return client
+}

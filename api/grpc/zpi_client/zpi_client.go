24ed1c702d4955936b658583f352655ac8cf68cc
zischl <supermechs456@gmail.com>
2026-03-26 22:37:30 +0530

    - Added ToF event stream handling with trigger-response workflow for   ZPiClient - Now connecting to ZPi GRPC host and start ToFEventStream - Listening for ToFEvent_TRIGGER and respond with PENDING then RESULT states - Simulating processing delays with time.Sleep - Added logging for connection, trigger, and response events - Handle stream errors gracefully

--- api/grpc/zpi_client/zpi_client.go
+++ api/grpc/zpi_client/zpi_client.go
@@ -1,14 +1,16 @@
 package zpi_client
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"github.com/Z-TAS-Solutions/ZScannerService/internal/pkg/zscanproto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func RunZPiClient(ip string) zscanproto.ZPiControllerClient {
+func RunZPiClient(ip string) (zscanproto.ZPiControllerClient, zscanproto.ZPiController_ToFEventStreamClient) {
 	conn, err := grpc.Dial(ip, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect: %v", err)
@@ -17,5 +19,46 @@ func RunZPiClient(ip string) zscanproto.ZPiControllerClient {
 	client := zscanproto.NewZPiControllerClient(conn)
 	log.Print("Connected to ZPi GRPC Host!")
 
-	return client
+	stream, err := client.ToFEventStream(context.Background())
+	if err != nil {
+		log.Fatalf("Failed To Start ToF Event Stream: %v", err)
+	}
+
+	go func() {
+		for {
+			evt, err := stream.Recv()
+			if err != nil {
+				log.Println("Server disconnected or error:", err)
+				return
+			}
+
+			if evt.Type == zscanproto.ToFEvent_TRIGGER {
+				log.Println("ToF trigger received!")
+
+				time.Sleep(2 * time.Second)
+
+				pendingState := &zscanproto.ToFEvent{
+					Type: zscanproto.ToFEvent_PENDING,
+				}
+				if err := stream.Send(pendingState); err != nil {
+					log.Println("Failed to send pending state:", err)
+					continue
+				}
+
+				time.Sleep(3 * time.Second)
+
+				response := &zscanproto.ToFEvent{
+					Type:      zscanproto.ToFEvent_RESULT,
+					LedStatus: zscanproto.LEDStatus_SUCCESS,
+				}
+				if err := stream.Send(response); err != nil {
+					log.Println("Failed to send response:", err)
+					continue
+				}
+				log.Println("Response sent !")
+			}
+		}
+	}()
+
+	return client, stream
 }
